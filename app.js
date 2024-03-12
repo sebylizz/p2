76d7be2c323b09b785af5b35fa9ba982ec44175a
emil624g <emil624g@gmail.com>
2024-03-12 21:05:49 +0100

    smukt ny main med cosine tilføjet

--- app.js
+++ app.js
@@ -2,32 +2,34 @@ const express = require('express');
 const path = require('path');
 const PORT = 3000;
 const app = express();
-const translater = require('./src/translate.js');               // opretter en konstant variabel som requirer translate.js
-const cosinus = require('./src/cosinus.js');                    // opretter en konstant variabel som requirer cosinus.js
-// const jaccard = require('./src/jaccard.js');                 // opretter en konstant variabel som requirer jaccard.js
+const translater = require('./src/translate');                                    // opretter en konstant function som requirer translate
+// const sanitizer = require('/src/saniterzerfraragnar');                         // opretter en konstant function som requirer saniterzerfraragnar
+const cosinus = require('./src/cosine');                                          // opretter en konstant function som requirer cosinus
+// const jaccard = require('./src/jaccard');                                      // opretter en konstant function som requirer jaccard
 // her kan nemt tilføjes flere når modulerne/algoritmerne er lavet
 
 app.use(express.json());
 
-app.set("views", path.join(__dirname, '/views'));               // opretter en direkte path til views for at fange front.html
+app.set("views", path.join(__dirname, '/views'));                                 // opretter en direkte path til views for at fange front.html
 app.engine('html', require('ejs').renderFile);
 app.set("view engine", "html");
-app.use(express.static(path.join(__dirname, 'public')));        // gør det der er i public mappen available for alle (no secrets)
+app.use(express.static(path.join(__dirname, 'public')));                          // gør det der er i public mappen available for alle (no secrets)
 
-app.get('/', (request, response) => {                           // requester en get fra front siden
-    response.render('front');                                   // render front.html vha en get
+app.get('/', (request, response) => {                                             // requester en get fra front siden
+    response.render('front');                                                     // render front.html vha en get
 })
 
-app.post('/', async(request, response) => {                     // requester en post request fra front siden
-    let answers = {};                                           // opretter et array
-    answers.translated = await translater(request.body.text);   // får translated text fra front fra translate.js og putter ind i object answers
-    // answers.cosinus = cosinus(answers.translated);           // får resultatsvar fra translated fra cosinus.js og putter ind i object answers
-    // answers.jaccard = jaccard(answers.translated);           // får resultatsvar fra translated fra jaccard.js og putter ind i object answers
+app.post('/', async(request, response) => {                                       // requester en post request fra front siden
+    let answers = {};                                                             // opretter et array
+    let translated = await translater(request.body.text);                         // får translated text fra front fra translate.js og putter ind i object answers
+    // answers.translated = sanitizer(translated);
+    answers.cosinus = cosinus([translated], ["Vores smukke database"]);           // får resultatsvar fra translated fra cosinus.js og putter ind i object answers
+    // answers.jaccard = jaccard(answers.translated);                             // får resultatsvar fra translated fra jaccard.js og putter ind i object answers
     // her kan nemt tilføjes flere når modulerne/algoritmerne er lavet
-    response.json(answers);                                     // sender et respons i json format i et array som hedder answers
+    response.json(answers);                                                       // sender et respons i json format i et array som hedder answers
 })
  
 app.listen(PORT, function(err){
     if (err) console.log("Error in server setup");
-    console.log("Server listening on Port", PORT);
+    console.log("Server listening on http://localhost:" + PORT);
 })
