5f13ac3eacf61c5c625f9c497593a7ba09fb635d
ragnarhansen <ragnar.hansen37@gmail.com>
2024-03-17 20:22:45 +0100

    Virker?

--- src/sanitizeinput.js
+++ src/sanitizeinput.js
@@ -1,3 +1,6 @@
+
+// funktion til at sanitize input fjerner tekst som følger "hej" og newline karakterer //
+
 function sanitizeinput(input){
     let sanitize = input.replace(/\r?\n|\r/g, '');
     sanitize = sanitize.replace(/"[^"]*"/g,'');
