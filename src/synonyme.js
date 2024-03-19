1ff9badcc229a6a0f8b6be238f18d98afb367bc0
emil624g <emil624g@gmail.com>
2024-03-19 19:36:04 +0100

    comments på synonym

--- src/synonyme.js
+++ src/synonyme.js
@@ -1,12 +1,16 @@
 const synonymedict = require('word-thesaurus');
 
+// function som kopierer en input sætning og tjekker om nogen ord kan udskiftes med en databasesætning hvis det er synonymer og derefter udskifter
 function wordreplacer(input, databasesætning){
 
+    // fjerner mellemrum og andre tegn + den laver alle ord til lowercase
     input = input.replace(/[^\w ]/g, '').toLowerCase().split(" ");
     databasesætning = databasesætning.replace(/[^\w ]/g, '').toLowerCase().split(" ");
 
+    // kopierer input over i et array
     let originalinput = [...input];
 
+    // replacer alle ord i begge sætninger som er ens med en tom string
     for (let i = 0; i < input.length; i++){
         for (let j = 0; j < databasesætning.length; j++){
             if (input[i] === databasesætning[j]){
@@ -16,6 +20,7 @@ function wordreplacer(input, databasesætning){
         }
     }
     
+    // fjerner alle tomme strings i begge sætninger
     let i = 0;
     while (i < input.length){
         if (input[i] === ""){
@@ -25,6 +30,7 @@ function wordreplacer(input, databasesætning){
         }
     }
 
+    // fjerner tomme strings
     i = 0;
     while (i < databasesætning.length){
         if (databasesætning[i] === ""){
@@ -34,6 +40,7 @@ function wordreplacer(input, databasesætning){
         }
     }
     
+    // tjekker om de to ord i arrayesne er synonymer med hinanden og udskifter ordet som er et synonym med index pladsen i originalinput
     for (let i = 0; i < input.length; i++){
         for (let j = 0; j < databasesætning.length; j++)
             if (synonyme(input[i], databasesætning[j])){
@@ -43,10 +50,12 @@ function wordreplacer(input, databasesætning){
             }
     }
 
+    // returner originalinput men i string form med mellemrum og ikke arrayform
     return originalinput.join(" ");
 
 }
 
+// funktion som tager 2 ord som input og returner false hvis den ikke kan finde ord 1 eller at det er tomt men returner true hvis den finder synonymer
 function synonyme(ord1, ord2){
 
     let tempord = synonymedict.search(ord1);
