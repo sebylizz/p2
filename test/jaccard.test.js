e0f9f0ee5b742842b6b22e0d066ee21aaaa7e499
emil624g <emil624g@gmail.com>
2024-04-15 12:56:37 +0200

    Går roligt fremad

--- test/jaccard.test.js
+++ test/jaccard.test.js
@@ -1,8 +1,8 @@
 const jaccard = require('../src/jaccard');
 
-describe("", () => {
-    test("", () => {
-        expect(jaccard()).toBe();
+describe("Testing jaccard file", () => {
+    test("Similarity between 2 sentences", () => {
+        expect(jaccard(["Kamala Harris approval rating high among Democrats, but no elsewhere"], ["Kamala Harris approval rating high among Dems but not elsewhere"])).toBe("63.64%");
     });
 
     test("", () => {
