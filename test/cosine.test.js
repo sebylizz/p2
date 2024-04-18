779f1f5f945a3291bc1a5b4b98a186b4e7f5b2db
emil624g <emil624g@gmail.com>
2024-04-18 21:56:11 +0200

    cosine test done

--- test/cosine.test.js
+++ test/cosine.test.js
@@ -4,11 +4,11 @@ const paragraphs = cosine. paragraphs;
 
 describe("Testing cosine file", () => {
     test("sentence function test", () => {
-        expect(sentence(["i love learning"], ["you i love"], [["i", 1], ["love", 1], ["learning", 1], ["you", 1], ["are", 1], ["tall", 1]])).toEqual([[0, 0, "81.65"]]);
+        expect(sentence(["i love learning"], ["you i love"], [["i", 1], ["love", 1], ["learning", 1], ["you", 1], ["are", 1], ["tall", 1]])).toEqual([[0, 0, "66.67"]]);
     });
 
     test("paragraph function test", () => {
-        expect(paragraphs("I wish i had some apples", [{content: "i wish i had some apples"}], [["this", 1], ["is", 1], ["a", 1], ["small", 1], ["sample", 1], ["text", 1], ["i", 1], ["wish", 1], ["had", 1], ["some", 1], ["apples", 1]])).toEqual(["100.00", 0]);
+        expect(paragraphs("i wish i had some apples", [{content: "apples had an impact on society"}], [])).toEqual(["28.87", 0]);
     });
 
 });
