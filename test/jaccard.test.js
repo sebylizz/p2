6e989ac1803397256d64cea3b3f0b54d05be5130
emilkjeldgaard <ekkr23@student.aau.dk>
2024-05-14 13:35:02 +0200

    penis

--- test/jaccard.test.js
+++ test/jaccard.test.js
@@ -4,11 +4,11 @@ const paragraphs = jaccard.jaccardSimilarity;
 
 describe("Testing jaccard file", () => {
     test("Similarity between 2 sentences", () => {
-        expect(sentences(["i want to be a superstar"], [["a superstar i want to be"]], [[0, 0, 0, 0]], [])).toEqual([[0, 0, 66.67, 0]]);
+        expect(sentences(["i want to be a superstar"], [["a superstar i want to be"]], [[0, 0, 0, 0]])).toEqual([[0, 0, 66.67, 0]]);
     });
 
     test("Testing jaccard article based", () => {
-        expect(paragraphs("i am not happy for blue cars", [{content: "blue cars is not happy for gravel"}], [])).toEqual([[33.33, 0]]);
+        expect(paragraphs("i am not happy for blue cars", [{content: "blue cars is not happy for gravel"}])).toEqual([[33.33, 0]]);
     });
 
 });
